47b4277cda54df8afbf5656ac0292016a09c63ad
Samu <castillocisternas1994@gmail.com>
2023-10-16 18:20:41 +0000

    js final

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 
-function colorAleatorio() {
+function colorAleatorio() { //funcion para color aleatorio
     const colors = ["green", "blue", "red"]; //0,1,2
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
 }
-
 
 function cambioColor() { // cambio el color de todos los h5
     const h5Tags = document.querySelectorAll("h5");
@@ -14,3 +13,7 @@ function cambioColor() { // cambio el color de todos los h5
         h5.style.color = randomColor;
     });
 }
+
+document.querySelectorAll("h5").forEach((h5) => {
+    h5.addEventListener("click", cambioColor);
+});

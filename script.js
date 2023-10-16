64d52c9d2a4f0cd4c4134be2c2b55654c773ce23
scocca <s.cocca.u@gmail.com>
2023-10-16 18:59:47 +0000

    proyecto debuggeado y corregido

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-
-<<<<<<< HEAD
-=======
-function colorAleatorio() {
+function colorAleatorio() { //funcion para color aleatorio
     const colors = ["green", "blue", "red"]; //0,1,2
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
 }
-
 
 function cambioColor() { // cambio el color de todos los h5
     const h5Tags = document.querySelectorAll("h5");
@@ -16,4 +12,7 @@ function cambioColor() { // cambio el color de todos los h5
         h5.style.color = randomColor;
     });
 }
->>>>>>> samu
+
+document.querySelectorAll("h5").forEach((h5) => {
+    h5.addEventListener("click", cambioColor);
+});
